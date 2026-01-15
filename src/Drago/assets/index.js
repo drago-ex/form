0b04f2589dcce5cbd035dca6c455412363499cba
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-01-15 11:50:41 +0100

    Update index.js

--- src/Drago/assets/index.js
+++ src/Drago/assets/index.js
@@ -1,6 +1,8 @@
 // CSS (side-effect import)
 import './password.scss';
+import './tom.select.scss';
 
 // JS exports
 export { default as PasswordToggle } from "./naja.password.js";
 export { default as SubmitButtonDisable } from "./naja.button.js";
+export { default as TomSelectExtension } from "./naja.tom.select.js";
