97c3eba6a37035dac93e63b682554519f76f1036
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-01-15 11:50:28 +0100

    Update naja.tom.select.js

--- src/Drago/assets/naja.tom.select.js
+++ src/Drago/assets/naja.tom.select.js
@@ -1,33 +1,24 @@
-import naja from "naja";
-import TomSelect from "tom-select";
+import TomSelect from 'tom-select';
 
-/**
- * Initializes TomSelect on elements with the class 'select-tom'.
- * If the element has the 'data-locked' attribute, the remove_button plugin is disabled.
- *
- * @param {ParentNode} el The parent element where TomSelect should be initialized.
- */
-function tomSelect(el) {
-	el.querySelectorAll('.select-tom').forEach(e => {
-		// Check if the element has the 'data-locked' attribute.
-		const isLocked = e.hasAttribute('data-locked');
+export default class TomSelectExtension {
+	initialize(naja) {
+		const tomSelect = (el) => {
+			el.querySelectorAll('.select-tom').forEach(e => {
+				const isLocked = e.hasAttribute('data-locked');
 
-		// Initialize TomSelect with the remove_button plugin unless it's locked.
-		e._tom = new TomSelect(e, {
-			plugins: isLocked ? [] : ['remove_button'],
+				e._tom = new TomSelect(e, {
+					plugins: isLocked ? [] : ['remove_button'],
+				});
+
+				if (isLocked) {
+					e._tom.lock();
+				}
+			});
+		};
+
+		tomSelect(document);
+		naja.snippetHandler.addEventListener('afterUpdate', (e) => {
+			tomSelect(e.detail.snippet);
 		});
-
-		// Lock the TomSelect instance if it's marked as locked.
-		if (isLocked) {
-			e._tom.lock();
-		}
-	});
+	}
 }
-
-// Initialize TomSelect when the DOM content is loaded.
-document.addEventListener('DOMContentLoaded', () => tomSelect(document));
-
-// Reinitialize TomSelect after updating content with Naja.
-naja.snippetHandler.addEventListener('afterUpdate', (e) => {
-	tomSelect(e.detail.snippet);
-});
