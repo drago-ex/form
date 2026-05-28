029d8447ac9989b26593a4c44d97da9196f309eb
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-28 06:21:32 +0200

    add extra class

---
+++ src/Drago/Form/FormsExtra.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drago\Form;
+
+use Nette\Forms\Form;
+
+
+/** Commonly used inputs. */
+class FormsExtra extends Forms
+{
+	/** Adds a password input field. */
+	public function addPasswordField(): Input
+	{
+		return $this->addPasswordInput('password', 'Password')
+			->setPlaceholder('Your password')
+			->setRequired('Please enter your password.');
+	}
+
+
+	/** Adds a password confirmation input field. */
+	public function addPasswordConfirmationField(): Input
+	{
+		$passwordField = $this->addPasswordInput('verify', 'Password to check')
+			->setPlaceholder('Re-enter password')
+			->setRequired('Please enter your password to check.');
+
+		if (!isset($this['password'])) {
+			throw new \InvalidArgumentException('Password field is required for password confirmation.');
+		}
+
+		$passwordField->addRule(Form::Equal, 'Passwords do not match.', $this['password']);
+		return $passwordField;
+	}
+
+
+	/** Adds an email input field. */
+	public function addEmailField(): Input
+	{
+		return $this->addEmailInput('email', 'Email')
+			->setPlaceholder('Email address')
+			->setRequired('Please enter your email address.')
+			->setAutocomplete(Autocomplete::Email)
+			->addRule(Form::Email);
+	}
+}
